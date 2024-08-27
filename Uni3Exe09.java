d13a4d4d8f7bcc25444bc40b2e9c9a6c50f957e1
Bieeltvz <110978689+Bieeltvz@users.noreply.github.com>
2024-08-27 19:31:11 -0300

    Update Uni3Exe09.java

--- Uni3Exe09.java
+++ Uni3Exe09.java
@@ -5,12 +5,11 @@ import java.util.Scanner;
 public class Uni3Exe09 {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
-        double pi = 3.14159;
         System.out.println("Qual o raio? ");
         double raio = teclado.nextDouble();
         System.out.println("Qual a altura?");
         double altura = teclado.nextDouble();
-        double volume = pi * Math.pow(raio, 2) * altura;
+        double volume = Math.PI * Math.pow(raio, 2) * altura;
         System.out.println("Volume de " + volume );
         teclado.close();
     }
